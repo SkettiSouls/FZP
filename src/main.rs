b5ca0c02abce70fc563f8fde30b4f66d50f6e92e
SkettiSouls <skettisouls@gmail.com>
2024-04-15 05:03:15 -0500

    Logic skeleton
    Most if not all of the logic that doesn't involve audio or fuzzy finding.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::path::PathBuf;
+use std::{env::set_current_dir, path::PathBuf, process::exit};
 use clap::Parser;
+
 
 #[derive(Debug, Parser)]
 #[command(version, about = "Fuzzy player", long_about = None)]
@@ -23,6 +24,9 @@ struct Cli {
     /// File extension to search by in playlist mode
     #[arg(long, short, default_value = "m3u")]
     extension: String,
+
+    /// Directory to search in [default: $HOME/Music]
+    path: Option<PathBuf>,
 }
 
 fn main() {
@@ -38,13 +42,54 @@ fn main() {
 }
 
 fn mode_directory() {
-    // directory logic
+    let cli = Cli::parse();
+    match cli {
+        Cli { directory: Some(args), .. } => {
+            // Assigns args to music_directory if a path is provided, and defaults to the xdg audio
+            // directory if no path is provided.
+            let music_directory = args.unwrap_or_else(|| dirs::audio_dir().expect("Failed to find default music directory."));
+
+            // Change to the provided directory.
+            set_current_dir(&music_directory).expect("Failed to change directory.");
+        },
+
+        // The compiler demands this arm be included, despite being unreachable.
+        // This arm is unreachable because the function `mode_directory()` CANNOT
+        // be run if directory is None. It MUST be `directory: Some(_)`.
+        Cli { directory: None, .. } => {
+            println!("Paradox encountered: Directory mode unset in directory mode.");
+            exit(1);
+        }
+    }
 }
 
 fn mode_fuzzy() {
-    // fuzzy logic
+    let cli = Cli::parse();
+    match cli {
+        Cli { path: Some(music_directory), .. } => {
+            set_current_dir(&music_directory).expect("Failed to change directory.");
+        },
+
+        Cli { path: None, .. } => {
+            let music_directory = dirs::audio_dir().expect("Failed to find default music directory.");
+
+            set_current_dir(&music_directory).expect("Failed to change directory.");
+        }
+    }
 }
 
 fn mode_playlist() {
-    // playlist logic
+    let cli = Cli::parse();
+    match cli {
+        Cli { playlist: Some(args), .. } => {
+            let music_directory = args.unwrap_or_else(|| dirs::audio_dir().expect("Failed to find default music directory."));
+
+            set_current_dir(&music_directory).expect("Failed to change directory.");
+        },
+
+        Cli { playlist: None, .. } => {
+            println!("Paradox encountered: Directory mode unset in directory mode.");
+            exit(1);
+        }
+    }
 }

d65277a5ffe3a0585c440ffffe735901815825d9
SkettiSouls <skettisouls@gmail.com>
2024-09-04 21:37:26 -0500

    Init
    Create dev shell
    Add rust build path (./target) to .gitignore

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs:
+    with inputs;
+      let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
+      in
+      {
+        devShells.x86_64-linux.default = pkgs.mkShell {
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+
+          nativeBuildInputs = with pkgs; [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ];
+        };
+      };
+}
